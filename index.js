c4751dfe51ecbaa48b23b18a7ce245758b356bae
camshaft <bytheway.cameron@gmail.com>
2014-11-10 13:19:06 -0700

    use ngIf instead of css class

--- index.js
+++ index.js
@@ -11,57 +11,120 @@ var feature = require('feature');
 
 var package = module.exports = angular.module('ng-feature', []);
 
+package.factory('feature', [
+  function() {
+    return {
+      watch: watch
+    };
+
+    function parseExpr(check, subs, expr) {
+      var parts = expr.split(/ *\! */);
+      var len = parts.length;
+      if (len > 2) throw new Error('What are you trying to do here? "' + expr + '"');
+      var not = len === 2;
+      var name = not ? parts[1] : parts[0];
+
+      var value = false;
+      subs.push(feature.watch(name, function(newVal) {
+        value = not ? !newVal : newVal;
+        check();
+      }));
+
+      return function() {return value;};
+    }
+
+    function parseAnd(check, subs, expr) {
+      var ands = expr.split(/ *\|\| */).map(parseExpr.bind(null, check, subs));
+      return function() {
+        for (var i = ands.length - 1; i >= 0; i--) {
+          if (!ands[i]()) return false;
+        }
+        return true;
+      };
+    }
+
+    function watch(expr, $scope, cb) {
+      if (typeof $scope === 'function') {
+        cb = $scope;
+        $scope = null;
+      }
+
+      var features = expr.split(/ *\|\| */);
+      var subs = [];
+      var ors = [];
+      ors = features.map(parseAnd.bind(null, check, subs));
+      check();
+
+      function check() {
+        for (var i = ors.length - 1; i >= 0; i--) {
+          if (ors[i]()) return cb(true);
+        }
+        return cb(false);
+      }
+
+      function unsubscribe() {
+        subs.forEach(function(unwatch) {
+          unwatch();
+        });
+        subs = [];
+      }
+
+      if ($scope) $scope.$on('$destroy', unsubscribe);
+      return unsubscribe;
+    }
+  }
+]);
+
 /**
  * Toogle a feature based on feature flags
  */
 
 package.directive('feature', [
-  function() {
+  '$compile',
+  'feature',
+  function($compile, feature) {
     return {
+      multiElement: true,
+      replace: false,
+      terminal: true,
+      priority: 1000,
       restrict: 'A',
-      link: function($scope, elem, attrs) {
-        elem.addClass('ng-feature-disabled');
-        var features = attrs.feature.split('||');
-        var subs = [];
-        var ors = [];
+      compile: function(tEl, tAttrs) {
+        var expr = tAttrs.feature;
+        var name = '$feature_' + generateName(expr);
+        var ifExpr = name;
 
-        ors = features.map(function(ands) {
-          ands = ands.split('&&').map(function(cond) {
-            var name = cond.trim();
-            var not = name.charAt(0) === '!';
-            if (not) name = name.substr(1);
-            subs.push(feature.watch(name, check));
-            return not
-              ? function() { return !feature(name); }
-              : function() { return feature(name); };
+        var ifStr = tAttrs.ngIf;
+        if (ifStr) ifExpr += ' && ' + ifStr;
+
+        tEl.attr('data-ng-if', ifExpr);
+        tEl.removeAttr('data-feature');
+        tEl.removeAttr('feature');
+
+        return function($scope, elem, attrs) {
+          $compile(elem)($scope);
+          feature.watch(expr, $scope, function(value) {
+            $scope[name] = value;
+            if (!$scope.$$phase) $scope.$digest();
           });
-
-          return function() {
-            for (var i = ands.length - 1; i >= 0; i--) {
-              if (!ands[i]()) return false;
-            }
-            return true;
-          };
-        });
-
-        check();
-
-        function check() {
-          for (var i = ors.length - 1; i >= 0; i--) {
-            if (ors[i]()) return elem.removeClass('ng-feature-disabled');
-          }
-          return elem.addClass('ng-feature-disabled');
         }
-
-        $scope.$on('$destroy', function() {
-          subs.forEach(function(unwatch) {
-            unwatch();
-          });
-        });
       }
     };
   }
 ]);
+
+/**
+ * Generate a unique name for the expression
+ */
+
+function generateName(expr) {
+  return expr
+    .replace(/!/g, ' not ')
+    .replace(/&&/g, ' and ')
+    .replace(/\|\|/g, ' or ')
+    .trim()
+    .replace(/[^a-zA-Z0-9]+/g, '_');
+}
 
 /**
  * Tell other modules how to find us
